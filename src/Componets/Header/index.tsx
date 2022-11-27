949f27491d3db8dfbc10b31c666361f6bfbfd992
DouglasFloro <douglasfloro33@gmail.com>
2022-11-27 13:13:10 -0300

    change header

--- src/Componets/Header/index.tsx
+++ src/Componets/Header/index.tsx
@@ -3,7 +3,37 @@ import * as S from './styles';
 export const Header: React.FC = () => {
   return (
     <S.Wrapper>
-      <S.Container></S.Container>
+      <S.Container>
+        <S.Topo>
+          <S.Logo />
+          <S.DetalheTopo />
+        </S.Topo>
+        <S.Navegacao>
+          <ul>
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li>
+              <a href="brasil.html">Brasil</a>
+            </li>
+            <li>
+              <a href="internacional.html">Internacional</a>
+            </li>
+            <li>
+              <a href="economia.html">Economia</a>
+            </li>
+            <li>
+              <a href="saude.html">Saúde</a>
+            </li>
+            <li>
+              <a href="ciencia.html">Ciência</a>
+            </li>
+            <li>
+              <a href="fotos.html">Fotos</a>
+            </li>
+          </ul>
+        </S.Navegacao>
+      </S.Container>
     </S.Wrapper>
   );
 };
--- src/Styles/global.ts
+++ src/Styles/global.ts
@@ -25,7 +25,7 @@ const GlobalStyles = createGlobalStyle`
     width: 100%;
     min-height: 100vh;
 
-
+    background: #fff;
 
   }
 `;
